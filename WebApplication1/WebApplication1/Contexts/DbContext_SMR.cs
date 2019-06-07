f9547adc2423db1e97e980d69d364e7e6ada5cbf
Mendel Riveros <Sergio.Mendel@sonda.com>
2019-06-07 09:13:28 -0400

    Primer API

---
+++ WebApplication1/WebApplication1/Contexts/DbContext_SMR.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Entities;
+
+namespace WebApplication1.Contexts
+{
+    public class DbContext_SMR: DbContext
+    {
+        public DbSet<Persona> TablaPersona { get; set; }
+        public DbContext_SMR(DbContextOptions<DbContext_SMR> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/PersonaController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WebApplication1.Contexts;
+using WebApplication1.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")] // Toma el control defindo
+    [ApiController]
+    public class PersonaController: ControllerBase
+    {
+        public DbContext_SMR Context_SMR { get; }
+    
+        public PersonaController(DbContext_SMR varContext)
+        {
+            Context_SMR = varContext;
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<Persona>> Get()
+        {
+            return Context_SMR.TablaPersona.ToList();
+        }
+        
+        [HttpGet("{id}", Name = "ObtenerPersona")]
+        public ActionResult<Persona> Get(int id)
+        {
+            var varPersona = Context_SMR.TablaPersona.FirstOrDefault(x => x.Id == id);
+
+            if (varPersona == null)
+            {
+                return NotFound();
+            }
+
+            return varPersona;
+
+        }
+        
+        [HttpPost]
+        public ActionResult Post([FromBody] Persona varPersona)
+        {
+            //ModelState no es necesario de netcore 2.1, por ejemplo si en la clase autor uso un [Required]
+            //if(!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+            Context_SMR.TablaPersona.Add(varPersona);
+            Context_SMR.SaveChanges();
+            return new CreatedAtRouteResult("ObtenerPersona", new { id = varPersona.Id }, varPersona);
+
+        }
+        
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Persona xPersona)
+        {
+            Context_SMR.Entry(xPersona).State = EntityState.Modified;
+            Context_SMR.SaveChanges();
+            return Ok();
+        }
+        
+        // DELETE api/autores/5
+        [HttpDelete("{id}")]
+        public ActionResult<Persona> Delete(int id)
+        {
+            var ElimPersona = Context_SMR.TablaPersona.FirstOrDefault(x => x.Id == id);
+
+            if (ElimPersona == null)
+            {
+                return NotFound();
+            }
+
+            Context_SMR.Remove(ElimPersona);
+            Context_SMR.SaveChanges();
+            return Ok(ElimPersona);
+        }
+
+    }
+}
